c9a8dae59d1f5d2541d7f3b09423bfca7d2f8038
Omar Hesham <omaraziz255@gmail.com>
2021-05-22 04:04:51 +0200

    Support for uploading files from anywhere on file system + saving file on server

--- website/Backend/app.py
+++ website/Backend/app.py
@@ -1,4 +1,5 @@
 # Importing required libraries.
+import os
 
 from flask import Flask, render_template, request, Response, redirect, url_for
 from werkzeug.utils import secure_filename
@@ -8,6 +9,8 @@ import subprocess
 import wave
 
 app = Flask(__name__)
+RESULTS_DIR = "results/"
+RESULT_FILE = "audio.wav"
 
 
 @app.route('/')
@@ -17,30 +20,32 @@ def upload_file():
 
 @app.route('/', methods=['GET', 'POST'])
 def uploaded_file():
-    dst = "results/audio.wav"
+    dst = RESULTS_DIR+RESULT_FILE
 
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
 
         f = request.files['file']
-        print(type(f), f)
         if f.filename == "":
             return redirect(request.url)
 
-        print(f.filename, ".mp3" in f.filename)
         path = "audio_dataset/" + f.filename
+        print(f.filename)
         if ".mp3" in f.filename:
             # convert mp3 to wav
-            sound = AudioSegment.from_mp3(path)
+            secure = secure_filename(f.filename)
+            f.save(secure)
+            sound = AudioSegment.from_mp3(secure)
             sound.export(dst, format="wav")
             play(sound)
+            os.remove(secure)
 
         elif "recorded_audio" in f.filename:
             subprocess.call(['ffmpeg', '-i', path, dst])
 
         else:
-            f.save(secure_filename(f.filename))
+            f.save(RESULTS_DIR + secure_filename(f.filename))
 
     return render_template('upload.html')
 

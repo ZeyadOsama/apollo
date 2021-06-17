5a3fba5cc8977080910766701bdf1acb1bdcbf13
Omar Hesham <omaraziz255@gmail.com>
2021-06-17 02:47:48 +0200

    Genre Classification Results View

--- website/backend/app.py
+++ website/backend/app.py
@@ -14,17 +14,27 @@ __license__ = "MIT License"
 import subprocess
 import wave
 
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, redirect, send_file,jsonify
 from flask_cors import CORS, cross_origin
 from pydub import AudioSegment
 from pydub.playback import play
 from werkzeug.utils import secure_filename
+import os, sys
+
+currDir = os.path.dirname(os.path.realpath(__file__))
+webDir = os.path.abspath(os.path.join(currDir, '..'))
+rootDir = os.path.abspath(os.path.join(webDir, '..'))
+if rootDir not in sys.path: # add parent dir to paths
+    sys.path.append(rootDir)
+
+from apollo.engine.models.GernreClassificaiton.tagger import *
 
 app = Flask(__name__)
 cors = CORS(app, expose_headers='Authorization')
 app.config['CORS_HEADERS'] = 'Content-Type'
 RESULTS_DIR = "results/"
 RESULT_FILE = "audio.wav"
+RESULT_MP3 = "audio.mp3"
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -38,12 +48,12 @@ def uploaded_file():
         print(f.filename)
         if ".mp3" in f.filename:
             # convert mp3 to wav
-            secure = secure_filename(f.filename)
+            secure = RESULTS_DIR + secure_filename(f.filename)
             f.save(secure)
-            sound = AudioSegment.from_mp3(secure)
+            os.rename(secure,RESULTS_DIR + RESULT_MP3)
+            sound = AudioSegment.from_mp3(RESULT_MP3)
             sound.export(dst, format="wav")
-            play(sound)
-            os.remove(secure)
+            # play(sound)
 
         elif "recorded_audio" in f.filename:
             subprocess.call(['ffmpeg', '-i', path, dst])
@@ -64,16 +74,7 @@ def read_wav_file(filename):
 
 
 @app.route('/TwoStems', methods=['GET'])
-def downloaded_file():
-    if request.method == 'GET':
-        path_to_file = "results/audio.wav"
-        return send_file(path_to_file,
-                         mimetype="audio/wav", as_attachment=True,
-                         attachment_filename="audio.wav")
-
-
-@app.route('/TwoStems', methods=['GET'])
-def downloaded_file():
+def downloaded_file_two():
     if request.method == 'GET':
         path_to_file = "results/audio.wav"
         return send_file(path_to_file,
@@ -82,7 +83,7 @@ def downloaded_file():
 
 
 @app.route('/FourStems', methods=['GET'])
-def downloaded_file():
+def downloaded_file_four():
     if request.method == 'GET':
         path_to_file = "results/audio.wav"
         return send_file(path_to_file,
@@ -91,7 +92,7 @@ def downloaded_file():
 
 
 @app.route('/FiveStems', methods=['GET'])
-def downloaded_file():
+def downloaded_file_five():
     if request.method == 'GET':
         path_to_file = "results/audio.wav"
         return send_file(path_to_file,
@@ -100,12 +101,16 @@ def downloaded_file():
 
 
 @app.route('/MusicTagging', methods=['GET'])
-def downloaded_file():
+def downloaded_file_tags():
     if request.method == 'GET':
-        path_to_file = "results/audio.wav"
-        return send_file(path_to_file,
-                         mimetype="audio/wav", as_attachment=True,
-                         attachment_filename="audio.wav")
+        print(request.args)
+        if "Tags" in request.args:
+            return jsonify(top_tags(RESULTS_DIR + RESULT_MP3))
+        else:
+            path_to_file = "results/audio.wav"
+            return send_file(path_to_file,
+                             mimetype="audio/wav", as_attachment=True,
+                             attachment_filename="audio.wav")
 
 
 if __name__ == '__main__':
--- website/frontend/src/components/MusicTagging.js
+++ website/frontend/src/components/MusicTagging.js
@@ -1,10 +1,18 @@
 ﻿import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import './Home.css';
+import axios from "axios";
 
 
 export class MusicTagging extends Component {
     static displayName = MusicTagging.name;
+    constructor(props) {
+        super();
+        this.state = {Tags: ""};
+        axios.get('http://localhost:5000/MusicTagging', {params: {"Tags": 1}}).then(resp => {
+            this.setState({Tags: resp.data});
+        });
+    }
 
 
 
@@ -22,6 +30,7 @@ export class MusicTagging extends Component {
                 </audio>
                 <hr />
                 <br />
+                <p> {this.state.Tags} </p>
 
             </Container>
             </div>

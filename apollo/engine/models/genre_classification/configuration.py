fafc43931d4f46561fe9dfa0c8f701044bdbe008
Marwan Elsafty <marwan.elsafty.ext@brightskiesinc.com>
2021-07-10 18:59:19 +0200

    Refactor 1

--- apollo/engine/models/genre_classification/configuration.py
+++ apollo/engine/models/genre_classification/configuration.py
@@ -8,12 +8,6 @@ N_MELS = 96
 BATCH_SIZE = 1  # (size of the batch during prediction)
 
 # Output labels
-MTT_LABELS = ['guitar', 'classical', 'slow', 'techno', 'strings', 'drums', 'electronic', 'rock', 'fast', 'piano',
-              'ambient', 'beat', 'violin', 'vocal', 'synth', 'female', 'indian', 'opera', 'male', 'singing', 'vocals',
-              'no vocals', 'harpsichord', 'loud', 'quiet', 'flute', 'woman', 'male vocal', 'no vocal', 'pop', 'soft',
-              'sitar', 'solo', 'man', 'classic', 'choir', 'voice', 'new age', 'dance', 'male voice', 'female vocal',
-              'beats', 'harp', 'cello', 'no voice', 'weird', 'country', 'metal', 'female voice', 'choral']
-
 MSD_LABELS = ['rock', 'pop', 'alternative', 'indie', 'electronic', 'female vocalists', 'dance', '00s',
               'alternative rock', 'jazz', 'beautiful', 'metal', 'chillout', 'male vocalists', 'classic rock', 'soul',
               'indie rock', 'Mellow', 'electronica', '80s', 'folk', '90s', 'chill', 'instrumental', 'punk', 'oldies',
